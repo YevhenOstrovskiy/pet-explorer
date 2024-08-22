8d1612c80cfb648232fa68ea7336ee8e10b3fd9d
Yevhen <steplerjeka@gmail.com>
2024-08-22 15:01:46 +0300

    domain

--- src/lib/fetchPets.ts
+++ src/lib/fetchPets.ts
@@ -1,9 +1,17 @@
 import { BaseUrls } from '@/constants';
 import { Pet } from '@/types/Pet';
+import { headers } from "next/headers";
+
+const headersList = headers();
+const domain = headersList.get("x-forwarded-host") || "";
+const protocol = headersList.get("x-forwarded-proto") || "";
+const pathname = headersList.get("x-invoke-path") || "";
+
+console.log('domain', domain);
 
 export const fetchPets = async (route: string): Promise<Pet[] | null> => {
   try {
-    const response = await fetch(`${BaseUrls.BASE_URL}/api${route}`);
+    const response = await fetch(`https://${domain}/api${route}`);
     if (!response.ok) {
       throw new Error(`Failed to fetch pets from ${route}`);
     }
