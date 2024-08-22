b874e271703d8f7e78859ee708c87644692beccd
Yevhen <steplerjeka@gmail.com>
2024-08-22 15:32:58 +0300

    domain

--- src/constants/base-urls.ts
+++ src/constants/base-urls.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+? `https://pet-explorer.vercel.app`
 : 'http://localhost:3000';
 export const BASE_CAT_URL = `https://api.thecatapi.com/v1/`;
 export const BASE_CAT_API_URL = `${BASE_CAT_URL}breeds/`;
