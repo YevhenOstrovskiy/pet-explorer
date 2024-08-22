96f8ce8de004f2462c11a74b71636298546a7adc
Yevhen <steplerjeka@gmail.com>
2024-08-22 12:45:27 +0300

    change url

--- src/constants/base-urls.ts
+++ src/constants/base-urls.ts
@@ -1,6 +1,6 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : "http://localhost:3000";
 export const BASE_CAT_URL = `https://api.thecatapi.com/v1/`;
 export const BASE_CAT_API_URL = `${BASE_CAT_URL}breeds/`;
